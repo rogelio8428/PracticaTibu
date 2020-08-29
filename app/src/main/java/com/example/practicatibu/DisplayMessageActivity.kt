bef820dd9470366ca4826c0df043a18b17fe1b0e
Tiburon <rogeliobai@gmail.com>
2020-08-29 16:03:34 -0300

    correcion

--- app/src/main/java/com/example/practicatibu/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/practicatibu/DisplayMessageActivity.kt
@@ -1,8 +1,8 @@
 package com.example.practicatibu
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_display_message.*
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +10,7 @@ class DisplayMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_message)
 
         val message = intent.getStringExtra("mensaje")
-        val saludo = findViewById<TextView>(R.id.TVMuestra).apply {
-            text = "Hola $message" }
+        TVMuestra.text = "Hola $message"
 
     }
 }
