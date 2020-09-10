620bfc18ecd6ef43321e1437bc87eae8ba9e7e5e
Tiburon <rogeliobai@gmail.com>
2020-09-10 15:21:48 -0300

    LogCat modificated

--- app/src/main/java/com/example/practicatibu/LogcatActivity.kt
+++ app/src/main/java/com/example/practicatibu/LogcatActivity.kt
@@ -5,39 +5,42 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class LogcatActivity : AppCompatActivity() {
+
+    private val TAG = LogcatActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logcat)
 
-        Log.i("MainActivity", R.string.create.toString())
+        Log.i(TAG, R.string.create.toString())
     }
     override fun onStart(){
         super.onStart()
-        Log.i("MainActivity", R.string.start.toString())
+        Log.i(TAG, R.string.start.toString())
     }
 
     override fun onPause(){
         super.onPause()
-        Log.i("MainActivity", R.string.pause.toString())
+        Log.i(TAG, R.string.pause.toString())
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i("MainActivity", R.string.restart.toString())
+        Log.i(TAG, R.string.restart.toString())
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i("MainActivity", R.string.resume.toString())
+        Log.i(TAG, R.string.resume.toString())
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("MainActivity", R.string.stop.toString())
+        Log.i(TAG, R.string.stop.toString())
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("MainActivity", R.string.destroy.toString())
+        Log.i(TAG, R.string.destroy.toString())
     }
 }
