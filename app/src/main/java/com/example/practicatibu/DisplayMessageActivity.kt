7b9851d792c09451ba125aa5f8b0ec1324da18fa
rogelio8428 <67703014+rogelio8428@users.noreply.github.com>
2020-09-11 00:55:13 -0300

    ImagenClickeable  (#7)
    * ImagenClickeable

--- app/src/main/java/com/example/practicatibu/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/practicatibu/DisplayMessageActivity.kt
@@ -1,16 +1,26 @@
 package com.example.practicatibu
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_message.*
 
 class DisplayMessageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
 
         val message = intent.getStringExtra(PARAM_MESSAGE)
         TVMuestra.text = "$message"
+    }
 
+    fun imageClickeable(view: View){
+     val visibility = when(descripcionImagen.visibility){
+         View.VISIBLE -> View.INVISIBLE
+         View.INVISIBLE -> View.VISIBLE
+         else -> View.VISIBLE
+     }
+        descripcionImagen.visibility = visibility
     }
 }
