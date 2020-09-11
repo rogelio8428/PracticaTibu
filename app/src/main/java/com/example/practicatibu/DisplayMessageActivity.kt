88b8e3b48ca598263651dd3cac1b81e4490358ee
Tiburon <rogeliobai@gmail.com>
2020-09-11 00:03:35 -0300

    ImagenClickeable (odio q se me clave el androidStudio)

--- app/src/main/java/com/example/practicatibu/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/practicatibu/DisplayMessageActivity.kt
@@ -1,10 +1,13 @@
 package com.example.practicatibu
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_display_message.*
 
+
 class DisplayMessageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
@@ -12,5 +15,16 @@ class DisplayMessageActivity : AppCompatActivity() {
         val message = intent.getStringExtra(PARAM_MESSAGE)
         TVMuestra.text = "$message"
 
+
+    }
+
+    fun imageClickeable(view: View){
+
+     if(descripcionImagen.visibility == View.VISIBLE) {
+         descripcionImagen.visibility = View.INVISIBLE
+     }
+        else{
+         descripcionImagen.visibility = View.VISIBLE
+     }
     }
 }
