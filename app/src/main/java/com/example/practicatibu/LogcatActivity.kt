1f139db40e06e308794ebd691ef8d5d1e2df8319
Tiburon <rogeliobai@gmail.com>
2020-09-14 22:47:04 -0300

    Date Picker aca creo desde la activity formulario hasta la creacion de los TV y la carga de la fecha con el Date Picker

--- app/src/main/java/com/example/practicatibu/LogcatActivity.kt
+++ app/src/main/java/com/example/practicatibu/LogcatActivity.kt
@@ -1,18 +1,40 @@
 package com.example.practicatibu
 
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_logcat.*
+import java.util.*
 
-class LogcatActivity : AppCompatActivity() {
+
+class LogcatActivity : AppCompatActivity(){
 
     private val TAG = LogcatActivity::class.java.simpleName
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logcat)
 
         Log.i(TAG, R.string.create.toString())
+
+        val calend = Calendar.getInstance()
+        val year = calend.get(Calendar.YEAR)
+        val month = calend.get(Calendar.MONTH)
+        val day = calend.get(Calendar.DAY_OF_MONTH)
+
+        button_date.setOnClickListener{
+            val dpd = DatePickerDialog( this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+                date_editText.setText("" + mDay + "/" + mMonth + "/" + mYear)
+            }, year, month, day)
+            dpd.show()
+        }
+
+
+
     }
     override fun onStart(){
         super.onStart()
@@ -43,4 +65,7 @@ class LogcatActivity : AppCompatActivity() {
         super.onDestroy()
         Log.i(TAG, R.string.destroy.toString())
     }
+
+
+
 }
